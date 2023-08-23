0e3cd0e8201aa164d956c513e3402a786d5ff6c1
Rolake <morolaanney@gmail.com>
2023-08-23 10:54:15 +0100

    Added custom autoscaling to the virtual machine scale set

--- Azure/vmss.tf
+++ Azure/vmss.tf
@@ -30,3 +30,66 @@ resource "azurerm_windows_virtual_machine_scale_set" "vmss" {
     }
   }
 }
+
+resource "azurerm_monitor_autoscale_setting" "vmss-autoscaling" {
+  name = "vmss-autoscaling"
+  resource_group_name = azurerm_resource_group.cloud-migration-resource-group.name
+  location = azurerm_resource_group.cloud-migration-resource-group.location
+  target_resource_id = azurerm_windows_virtual_machine_scale_set.vmss.id
+
+  profile {
+    name = "defaultProfile"
+
+    capacity {
+      default = 3
+      minimum = 3
+      maximum = 5
+    }
+
+    rule {
+      metric_trigger {
+        metric_name = "Percentage CPU"
+        metric_resource_id = azurerm_windows_virtual_machine_scale_set.vmss.id
+        time_grain = "PT1M"
+        statistic = "Average"
+        time_window = "PT5M"
+        time_aggregation = "Average"
+        operator = "GreaterThan"
+        threshold = 70
+        metric_namespace   = "microsoft.compute/virtualmachinescalesets"
+        # dimensions {
+        #   name     = "AppName"
+        #   operator = "Equals"
+        #   values   = ["App1"]
+        # }
+      }
+
+      scale_action {
+        direction = "Increase"
+        type = "ChangeCount"
+        value = "1" #Scale up by one instance
+        cooldown = "PT5M" #Cool down period - 5 minutes
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name = "Percentage CPU"
+        metric_resource_id = azurerm_windows_virtual_machine_scale_set.vmss.id
+        time_grain = "PT1M"
+        statistic = "Average"
+        time_window = "PT5M"
+        time_aggregation = "Average"
+        operator = "LessThan"
+        threshold = 25
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type = "ChangeCount"
+        value = "1"
+        cooldown = "PT5M"
+      }
+    }
+  }
+}
